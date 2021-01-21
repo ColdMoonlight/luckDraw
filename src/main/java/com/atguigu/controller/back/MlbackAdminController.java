6808120a60ebabf7bf1881b328737a67787936e0
Administrator <Administrator@PC-20201231AMTU>
2021-01-21 17:06:02 +0800

    aaa

--- src/main/java/com/atguigu/controller/back/MlbackAdminController.java
+++ src/main/java/com/atguigu/controller/back/MlbackAdminController.java
@@ -10,18 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-//import com.atguigu.bean.MlbackAdmin;
+import com.atguigu.bean.MlbackAdmin;
 import com.atguigu.common.Const;
 import com.atguigu.common.Msg;
-//import com.atguigu.common.TokenCache;
-//import com.atguigu.service.MlbackAdminService;
+import com.atguigu.service.MlbackAdminService;
 
 @Controller
 @RequestMapping("/MlbackAdmin")
 public class MlbackAdminController {
 	
-//	@Autowired
-//	MlbackAdminService mlbackAdminService;
+	@Autowired
+	MlbackAdminService mlbackAdminService;
 	
 	/**
 	 * 1.0
@@ -37,66 +36,46 @@ public class MlbackAdminController {
 		return "back/mlbackAdminLogin";
 	}
 	
-//	/**
-//	 * 2.0
-//	 * @author zsh
-//	 * @param MlbackAdmin
-//	 * @exception 管理员帐号登陆
-//	 * 20200428
-//	 * */
-//	@RequestMapping(value="/CheakAdminUser",method=RequestMethod.POST)
-//	@ResponseBody
-//	public Msg checkuser(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
-//			@RequestBody MlbackAdmin MlbackAdminReq){
-//		//接收参数信息 
-//		MlbackAdmin mlbackAdminGet = new MlbackAdmin();
-//		mlbackAdminGet.setAdminAccname(MlbackAdminReq.getAdminAccname());
-//		List<MlbackAdmin> mlbackAdminGetresList = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
-//		if(!(mlbackAdminGetresList.size()>0)){
-//			return Msg.fail().add("resMsg", "账号不存在");
-//		}
-//		mlbackAdminGet.setAdminPassword(MlbackAdminReq.getAdminPassword());
-//		List<MlbackAdmin> MlbackAdminListNameAndPwd = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
-//		if(MlbackAdminListNameAndPwd.size()>0){
-//			//将登陆状态放入session对象
-//			session.setAttribute(Const.ADMIN_USER, mlbackAdminGet);
-//			TokenCache.setKey(Const.TOKEN_PREFIX+MlbackAdminReq.getAdminAccname(), "String");
-//			return Msg.success().add("resMsg", "登陆成功");
-//		}else{
-//			return Msg.fail().add("resMsg", "密码错误登录失败");
-//		}
-//	}
-//	
-//	
-//	/**
-//	 * 3.0
-//	 * @author zsh
-//	 * @param MlbackAdmin
-//	 * @exception 管理员帐号修改密码
-//	 * 20200429
-//	 * */
-//	@RequestMapping(value="/UpdateAdminUserInfo",method=RequestMethod.POST)
-//	@ResponseBody
-//	public Msg UpdateAdminUserInfo(HttpServletResponse rep,HttpServletRequest res,HttpSession session,@RequestBody MlbackAdmin MlbackAdminReq){
-//		//1接收参数
-//		//2用账户+旧密码查账户(查到,update新密码,没查到,旧密码不对)
-//		MlbackAdmin mlbackAdminGet = new MlbackAdmin();
-//		mlbackAdminGet.setAdminAccname(MlbackAdminReq.getAdminAccname());
-//		List<MlbackAdmin> mlbackAdminGetresList = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
-//		if(!(mlbackAdminGetresList.size()>0)){
-//			return Msg.fail().add("resMsg", "账号不存在");
-//		}
-//		mlbackAdminGet.setAdminPassword(MlbackAdminReq.getAdminPassword());
-//		List<MlbackAdmin> MlbackAdminListNameAndPwd = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
-//		if(MlbackAdminListNameAndPwd.size()>0){
-//			//mlbackAdminGet
-//			mlbackAdminGet.setAdminPassword(MlbackAdminReq.getAdminOperatername());
-//			mlbackAdminService.updateByAdminAccnameSelective(mlbackAdminGet);
-//			session.setAttribute(Const.ADMIN_USER, mlbackAdminGet);
-//			return Msg.success().add("resMsg", "密码修改成功");
-//		}else{
-//			return Msg.fail().add("resMsg", "旧密码错误");
-//		}
-//	}
+	
+	/**
+	 * 1.0
+	 * @author zsh
+	 * @param MlbackAdmin
+	 * @exception 管理员帐号推出
+	 * 20200428
+	 * */
+	@RequestMapping("/tologin")
+	public String tologin(HttpSession session) throws Exception{
+		return "back/mlbackAdminLogin";
+	}
+	
+	/**
+	 * 2.0
+	 * @author zsh
+	 * @param MlbackAdmin
+	 * @exception 管理员帐号登陆
+	 * 20200428
+	 * */
+	@RequestMapping(value="/CheakAdminUser",method=RequestMethod.POST)
+	@ResponseBody
+	public Msg checkuser(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
+			@RequestBody MlbackAdmin MlbackAdminReq){
+		//接收参数信息 
+		MlbackAdmin mlbackAdminGet = new MlbackAdmin();
+		mlbackAdminGet.setAdminAccname(MlbackAdminReq.getAdminAccname());
+		List<MlbackAdmin> mlbackAdminGetresList = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
+		if(!(mlbackAdminGetresList.size()>0)){
+			return Msg.fail().add("resMsg", "账号不存在");
+		}
+		mlbackAdminGet.setAdminPassword(MlbackAdminReq.getAdminPassword());
+		List<MlbackAdmin> MlbackAdminListNameAndPwd = mlbackAdminService.selectMlbackAdmin(mlbackAdminGet);
+		if(MlbackAdminListNameAndPwd.size()>0){
+			//将登陆状态放入session对象
+			session.setAttribute(Const.ADMIN_USER, mlbackAdminGet);
+			return Msg.success().add("resMsg", "登陆成功");
+		}else{
+			return Msg.fail().add("resMsg", "密码错误登录失败");
+		}
+	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,8 +5,28 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+
+<%
+	pageContext.setAttribute("APP_PATH", request.getContextPath());
+%>
+<!-- web路径：
+不以/开始的相对路径，找资源，以当前资源的路径为基准，经常容易出问题。
+以/开始的相对路径，找资源，以服务器的路径为标准(http://localhost:3306)；需要加上项目名
+		http://localhost:3306/crud
+-->
+<script type="text/javascript" src="${APP_PATH }/static/js/jquery-1.12.4.min.js"></script>
+
 </head>
 <body>
 抽奖转盘1.0
 </body>
+
+
+<script>
+	//接口初始hua
+	  $(function() {
+		window.location.href="${APP_PATH}/login.html";
+		//启动index页面，调用接口判断请求来源 
+	}) 
+</script>
 </html>
