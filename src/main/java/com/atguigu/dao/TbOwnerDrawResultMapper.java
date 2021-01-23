68622632761dd8d19f01fb50169fc79ac06d9ad7
Administrator <Administrator@PC-20201231AMTU>
2021-01-23 11:03:25 +0800

    update

---
+++ src/main/java/com/atguigu/dao/TbOwnerDrawResultMapper.java
@@ -0,0 +1,24 @@
+package com.atguigu.dao;
+
+import java.util.List;
+
+import com.atguigu.bean.TbOwnerDrawResult;
+
+public interface TbOwnerDrawResultMapper {
+
+    int insert(TbOwnerDrawResult record);
+
+    TbOwnerDrawResult selectByPrimaryKey(Integer drawresultId);
+
+    int updateByPrimaryKey(TbOwnerDrawResult record);
+	
+	int insertSelective(TbOwnerDrawResult record);
+	
+	int deleteByPrimaryKey(Integer drawresultId);
+	
+	int updateByPrimaryKeySelective(TbOwnerDrawResult record);
+	
+	int updateAllDrawResultToGradezero();
+
+	List<TbOwnerDrawResult> selectTbOwnerDrawResultAll(TbOwnerDrawResult mlbackAdmin);
+}
---
+++ src/main/java/com/atguigu/service/TbOwnerDrawResultService.java
@@ -0,0 +1,60 @@
+package com.atguigu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.atguigu.bean.MlbackAdmin;
+import com.atguigu.bean.TbOwnerDrawResult;
+import com.atguigu.dao.TbOwnerDrawResultMapper;
+
+@Service
+public class TbOwnerDrawResultService {
+	
+	@Autowired
+	TbOwnerDrawResultMapper tbOwnerDrawResultMapper;
+
+	/**
+	 * @author Shinelon
+	 * @param TbOwnerDrawResult
+	 * @exception add方法用户信息是否存在
+	 * 	3.0
+	 * */
+	public int insertSelective(TbOwnerDrawResult TbOwnerDrawResult) {
+		int intReslut = tbOwnerDrawResultMapper.insertSelective(TbOwnerDrawResult);
+		return intReslut;
+	}
+	
+	/**
+	 * @author
+	 * @param 
+	 * @exception 删除本条信息
+	 * 	3.0
+	 * */
+	public int deleteByPrimaryKey(int CatalogId) {
+		int  intReslut = tbOwnerDrawResultMapper.deleteByPrimaryKey(CatalogId);
+		return intReslut;
+	}
+
+	/**
+	 * @author	3.0
+	 * @param 
+	 * @exception 更新本条信息
+	 * */
+	public int updateByPrimaryKeySelective(TbOwnerDrawResult TbOwnerDrawResult) {
+		int  intReslut = tbOwnerDrawResultMapper.updateByPrimaryKeySelective(TbOwnerDrawResult);
+		return intReslut;
+	}
+
+	public int updateAllDrawResultToGradezero() {
+		int  intReslut = tbOwnerDrawResultMapper.updateAllDrawResultToGradezero();
+		return intReslut;
+	}
+	
+	public List<TbOwnerDrawResult> selectTbOwnerDrawResultAll(TbOwnerDrawResult mlbackAdmin) {
+		List<TbOwnerDrawResult>  TbOwnerDrawResultList = tbOwnerDrawResultMapper.selectTbOwnerDrawResultAll(mlbackAdmin);
+		return TbOwnerDrawResultList;
+	}
+
+}
